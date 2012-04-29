acd638b62fa7692d29ac3340dd62411d94e9783b
Robby Neale <git@robertneale.com>
2012-04-29 11:53:15 -0700

    cleanup discoverSong method

--- soul-sifter/MusicManager.mm
+++ soul-sifter/MusicManager.mm
@@ -82,6 +82,23 @@
             case ID3FID_DATE: {
                 char *sText = ID3_GetString(frame, ID3FN_TEXT);
                 NSString *tmp = [song releaseDate];
+                // reverse to MMDD format
+                char day1 = sText[0];
+                if (day1) {
+                    char day2 = sText[1];
+                    if (day2) {
+                        char m1 = sText[2];
+                        if (m1) {
+                            char m2 = sText[3];
+                            if (m2) {
+                                sText[0] = m1;
+                                sText[1] = m2;
+                                sText[2] = day1;
+                                sText[3] = day2;
+                            }
+                        }
+                    }
+                }
                 if (tmp) {
                     [song setReleaseDate:[NSString stringWithFormat:@"%@%@",
                                           [song releaseDate], tmp]];
@@ -114,6 +131,7 @@
                 NSLog(@" - frame %s (%s): %@", frame->GetTextID(), desc, [song label]);
                 break;
             }
+            // the rest will just log messages
             case ID3FID_BPM:
             case ID3FID_COMPOSER:
             case ID3FID_CONTENTTYPE:
@@ -144,28 +162,24 @@
             case ID3FID_ISRC:
             case ID3FID_ENCODERSETTINGS:
             case ID3FID_USERTEXT: {
-                    char 
-                    *sText = ID3_GetString(frame, ID3FN_TEXT), 
-                    *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION);
-                    std::cout << "(" << sDesc << "): " << sText << std::endl;
-                    delete [] sText;
-                    delete [] sDesc;
-                    break;
-                }
+                char *sText = ID3_GetString(frame, ID3FN_TEXT),
+                     *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION);
+                NSLog(@" - frame %s (%s): %s", frame->GetTextID(), sDesc, sText);
+                delete [] sText;
+                delete [] sDesc;
+                break;
+            }
             case ID3FID_COMMENT:
-            case ID3FID_UNSYNCEDLYRICS:
-                {
-                    char 
-                    *sText = ID3_GetString(frame, ID3FN_TEXT), 
-                    *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION), 
-                    *sLang = ID3_GetString(frame, ID3FN_LANGUAGE);
-                    std::cout << "(" << sDesc << ")[" << sLang << "]: "
-                    << sText << std::endl;
-                    delete [] sText;
-                    delete [] sDesc;
-                    delete [] sLang;
-                    break;
-                }
+            case ID3FID_UNSYNCEDLYRICS: {
+                char *sText = ID3_GetString(frame, ID3FN_TEXT), 
+                     *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION), 
+                     *sLang = ID3_GetString(frame, ID3FN_LANGUAGE);
+                NSLog(@" - frame %s (%s)[%s]: %s", frame->GetTextID(), sDesc, sLang, sText);
+                delete [] sText;
+                delete [] sDesc;
+                delete [] sLang;
+                break;
+            }
             case ID3FID_WWWAUDIOFILE:
             case ID3FID_WWWARTIST:
             case ID3FID_WWWAUDIOSOURCE:
@@ -173,171 +187,148 @@
             case ID3FID_WWWCOPYRIGHT:
             case ID3FID_WWWPUBLISHER:
             case ID3FID_WWWPAYMENT:
-            case ID3FID_WWWRADIOPAGE:
-            {
+            case ID3FID_WWWRADIOPAGE: {
                 char *sURL = ID3_GetString(frame, ID3FN_URL);
-                std::cout << sURL << std::endl;
+                NSLog(@" - frame %s (%s): %s", frame->GetTextID(), desc, sURL);
                 delete [] sURL;
                 break;
             }
-            case ID3FID_WWWUSER:
-            {
-                char 
-                *sURL = ID3_GetString(frame, ID3FN_URL),
-                *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION);
-                std::cout << "(" << sDesc << "): " << sURL << std::endl;
+            case ID3FID_WWWUSER: {
+                char *sURL = ID3_GetString(frame, ID3FN_URL),
+                     *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION);
+                NSLog(@" - frame %s (%s): %s", frame->GetTextID(), sDesc, sURL);
                 delete [] sURL;
                 delete [] sDesc;
                 break;
             }
-            case ID3FID_INVOLVEDPEOPLE:
-            {
+            case ID3FID_INVOLVEDPEOPLE: {
                 size_t nItems = frame->GetField(ID3FN_TEXT)->GetNumTextItems();
-                for (size_t nIndex = 0; nIndex < nItems; nIndex++)
-            {
-                        char *sPeople = ID3_GetString(frame, ID3FN_TEXT, nIndex);
-                std::cout << sPeople;
-                        delete [] sPeople;
-                    if (nIndex + 1 < nItems)
-                    {
-                        std::cout << ", ";
+                NSString *ppl = @"";
+                for (size_t nIndex = 0; nIndex < nItems; nIndex++) {
+                    char *sPeople = ID3_GetString(frame, ID3FN_TEXT, nIndex);
+                    if (nIndex + 1 < nItems) {
+                        ppl = [NSString stringWithFormat:@"%@%s,", ppl, sPeople];
+                    } else {
+                        ppl = [NSString stringWithFormat:@"%@%s", ppl, sPeople];
                     }
-            }
-                    std::cout << std::endl;
-            break;
+                    delete [] sPeople;
                 }
-            case ID3FID_PICTURE:
-            {
-                char
-                *sMimeType = ID3_GetString(frame, ID3FN_MIMETYPE),
-                *sDesc     = ID3_GetString(frame, ID3FN_DESCRIPTION),
-                *sFormat   = ID3_GetString(frame, ID3FN_IMAGEFORMAT);
-                size_t
-                nPicType   = frame->GetField(ID3FN_PICTURETYPE)->Get(),
-                nDataSize  = frame->GetField(ID3FN_DATA)->Size();
+                NSLog(@" - frame %s (%s): %@", frame->GetTextID(), desc, ppl);
+                break;
+            }
+            case ID3FID_PICTURE: {
+                char *sMimeType = ID3_GetString(frame, ID3FN_MIMETYPE),
+                     *sDesc     = ID3_GetString(frame, ID3FN_DESCRIPTION),
+                     *sFormat   = ID3_GetString(frame, ID3FN_IMAGEFORMAT);
+                size_t nPicType   = frame->GetField(ID3FN_PICTURETYPE)->Get(),
+                       nDataSize  = frame->GetField(ID3FN_DATA)->Size();
                 std::cout << "(" << sDesc << ")[" << sFormat << ", "
                 << nPicType << "]: " << sMimeType << ", " << nDataSize
                 << " bytes" << std::endl;
+                NSLog(@" - frame %s (%s): %s, %lu: %s, %lu bytes", frame->GetTextID(), sDesc,
+                      sFormat, nPicType, sMimeType, nDataSize);
                 delete [] sMimeType;
                 delete [] sDesc;
                 delete [] sFormat;
                 break;
             }
-            case ID3FID_GENERALOBJECT:
-            {
-                char 
-                *sMimeType = ID3_GetString(frame, ID3FN_MIMETYPE), 
-                *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION), 
-                *sFileName = ID3_GetString(frame, ID3FN_FILENAME);
-                size_t 
-                nDataSize = frame->GetField(ID3FN_DATA)->Size();
-                std::cout << "(" << sDesc << ")[" 
-                << sFileName << "]: " << sMimeType << ", " << nDataSize
-                << " bytes" << std::endl;
+            case ID3FID_GENERALOBJECT: {
+                char *sMimeType = ID3_GetString(frame, ID3FN_MIMETYPE), 
+                     *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION), 
+                     *sFileName = ID3_GetString(frame, ID3FN_FILENAME);
+                size_t nDataSize = frame->GetField(ID3FN_DATA)->Size();
+                NSLog(@" - frame %s (%s): %s: %s, %lu bytes", frame->GetTextID(), sDesc,
+                      sFileName, sMimeType, nDataSize);
                 delete [] sMimeType;
                 delete [] sDesc;
                 delete [] sFileName;
                 break;
             }
-            case ID3FID_UNIQUEFILEID:
-            {
+            case ID3FID_UNIQUEFILEID: {
                 char *sOwner = ID3_GetString(frame, ID3FN_OWNER);
                 size_t nDataSize = frame->GetField(ID3FN_DATA)->Size();
-                std::cout << sOwner << ", " << nDataSize
-                << " bytes" << std::endl;
+                NSLog(@" - frame %s (%s): %s, %lu bytes", frame->GetTextID(), desc, sOwner, nDataSize);
                 delete [] sOwner;
                 break;
             }
-            case ID3FID_PLAYCOUNTER:
-            {
+            case ID3FID_PLAYCOUNTER: {
                 size_t nCounter = frame->GetField(ID3FN_COUNTER)->Get();
-                std::cout << nCounter << std::endl;
+                NSLog(@" - frame %s (%s): %lu", frame->GetTextID(), desc, nCounter);
                 break;
             }
-            case ID3FID_POPULARIMETER:
-        {
-                    char *sEmail = ID3_GetString(frame, ID3FN_EMAIL);
-                size_t
-                nCounter = frame->GetField(ID3FN_COUNTER)->Get(),
-            nRating = frame->GetField(ID3FN_RATING)->Get();
-                    std::cout << sEmail << ", counter=" 
-            << nCounter << " rating=" << nRating << std::endl;
-                    delete [] sEmail;
+            case ID3FID_POPULARIMETER: {
+                char *sEmail = ID3_GetString(frame, ID3FN_EMAIL);
+                size_t nCounter = frame->GetField(ID3FN_COUNTER)->Get(),
+                       nRating = frame->GetField(ID3FN_RATING)->Get();
+                NSLog(@" - frame %s (%s): %s, counter=%lu, rating=%lu", frame->GetTextID(), desc,
+                      sEmail, nCounter, nRating);
+                delete [] sEmail;
                 break;
             }
             case ID3FID_CRYPTOREG:
-            case ID3FID_GROUPINGREG:
-            {
+            case ID3FID_GROUPINGREG: {
                 char *sOwner = ID3_GetString(frame, ID3FN_OWNER);
-                size_t 
-                nSymbol = frame->GetField(ID3FN_ID)->Get(),
-                nDataSize = frame->GetField(ID3FN_DATA)->Size();
-                std::cout << "(" << nSymbol << "): " << sOwner
-                << ", " << nDataSize << " bytes" << std::endl;
+                size_t nSymbol = frame->GetField(ID3FN_ID)->Get(),
+                       nDataSize = frame->GetField(ID3FN_DATA)->Size();
+                NSLog(@" - frame %s (%s): %s (%lu), %lu bytes", frame->GetTextID(), desc,
+                      sOwner, nSymbol, nDataSize);
                 break;
             }
-            case ID3FID_SYNCEDLYRICS:
-            {
-                char 
-                *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION), 
-                *sLang = ID3_GetString(frame, ID3FN_LANGUAGE);
-                size_t
-                nTimestamp = frame->GetField(ID3FN_TIMESTAMPFORMAT)->Get(),
-                nRating = frame->GetField(ID3FN_CONTENTTYPE)->Get();
+            case ID3FID_SYNCEDLYRICS: {
+                char *sDesc = ID3_GetString(frame, ID3FN_DESCRIPTION), 
+                     *sLang = ID3_GetString(frame, ID3FN_LANGUAGE);
+                size_t nTimestamp = frame->GetField(ID3FN_TIMESTAMPFORMAT)->Get(),
+                       nRating = frame->GetField(ID3FN_CONTENTTYPE)->Get();
                 const char* format = (2 == nTimestamp) ? "ms" : "frames";
-                std::cout << "(" << sDesc << ")[" << sLang << "]: ";
-                switch (nRating)
-                {
-                    case ID3CT_OTHER:    std::cout << "Other"; break;
-                    case ID3CT_LYRICS:   std::cout << "Lyrics"; break;
-                    case ID3CT_TEXTTRANSCRIPTION:     std::cout << "Text transcription"; break;
-                    case ID3CT_MOVEMENT: std::cout << "Movement/part name"; break;
-                    case ID3CT_EVENTS:   std::cout << "Events"; break;
-                    case ID3CT_CHORD:    std::cout << "Chord"; break;
-                    case ID3CT_TRIVIA:   std::cout << "Trivia/'pop up' information"; break;
+                NSString *type;
+                switch (nRating) {
+                    case ID3CT_OTHER:    type = @"Other"; break;
+                    case ID3CT_LYRICS:   type = @"Lyrics"; break;
+                    case ID3CT_TEXTTRANSCRIPTION:     type = @"Text transcription"; break;
+                    case ID3CT_MOVEMENT: type = @"Movement/part name"; break;
+                    case ID3CT_EVENTS:   type = @"Events"; break;
+                    case ID3CT_CHORD:    type = @"Chord"; break;
+                    case ID3CT_TRIVIA:   type = @"Trivia/'pop up' information"; break;
                 }
-                std::cout << std::endl;
+                NSLog(@" - frame %s (%s)[%s]: %@: %lu %s", frame->GetTextID(), sDesc,
+                      sLang, type, nTimestamp, format);
                 ID3_Field* fld = frame->GetField(ID3FN_DATA);
-                if (fld)
-                {
-                    ID3_MemoryReader mr(fld->GetRawBinary(), fld->BinSize());
-                    while (!mr.atEnd())
-                    {
-                        //std::cout << io::readString(mr).c_str();
-                            //std::cout << " [" << io::readBENumber(mr, sizeof(uint32)) << " " 
-                            //<< format << "] ";
-                        }
-                    }
-                    std::cout << std::endl;
-                    delete [] sDesc;
-                    delete [] sLang;
-                    break;
+                if (fld) {
+                    // skip reading this
+                    /*ID3_MemoryReader mr(fld->GetRawBinary(), fld->BinSize());
+                    while (!mr.atEnd()) {
+                        std::cout << io::readString(mr).c_str();
+                        std::cout << " [" << io::readBENumber(mr, sizeof(uint32)) << " " 
+                                  << format << "] ";
+                    }*/
                 }
-                case ID3FID_AUDIOCRYPTO:
-                case ID3FID_EQUALIZATION:
-                case ID3FID_EVENTTIMING:
-                case ID3FID_CDID:
-                case ID3FID_MPEGLOOKUP:
-                case ID3FID_OWNERSHIP:
-                case ID3FID_PRIVATE:
-                case ID3FID_POSITIONSYNC:
-                case ID3FID_BUFFERSIZE:
-                case ID3FID_VOLUMEADJ:
-                case ID3FID_REVERB:
-                case ID3FID_SYNCEDTEMPO:
-                case ID3FID_METACRYPTO:
-                {
-                    std::cout << " (unimplemented)" << std::endl;
-                    break;
-                }
-                default:
-                {
-                    std::cout << " frame" << std::endl;
-                    break;
-                }
+                delete [] sDesc;
+                delete [] sLang;
+                break;
+            }
+            case ID3FID_AUDIOCRYPTO:
+            case ID3FID_EQUALIZATION:
+            case ID3FID_EVENTTIMING:
+            case ID3FID_CDID:
+            case ID3FID_MPEGLOOKUP:
+            case ID3FID_OWNERSHIP:
+            case ID3FID_PRIVATE:
+            case ID3FID_POSITIONSYNC:
+            case ID3FID_BUFFERSIZE:
+            case ID3FID_VOLUMEADJ:
+            case ID3FID_REVERB:
+            case ID3FID_SYNCEDTEMPO:
+            case ID3FID_METACRYPTO: {
+                NSLog(@" - frame %s (%s): unimplemented", frame->GetTextID(), desc);
+                break;
+            }
+            default: {
+                NSLog(@" - frame %s: unknown", frame->GetTextID());
+                break;
             }
         }
-        delete iter;
+    }
+    delete iter;
     
     return song;
 }
