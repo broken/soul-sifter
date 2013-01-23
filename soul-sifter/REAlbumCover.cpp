1a8be38ef550de54b83f9819cbe4debb1ab91291
Robby Neale <git@robertneale.com>
2013-01-22 23:42:30 -0800

    reAlbumCoverHelper

--- soul-sifter/REAlbumCover.cpp
+++ soul-sifter/REAlbumCover.cpp
@@ -72,13 +72,6 @@ namespace soulsifter {
         return ac;
     }
     
-    REAlbumCover::REAlbumCoverIterator* REAlbumCover::findAll() {
-        sql::PreparedStatement *ps = MysqlAccess::getInstance().getPreparedStatement("select * from REAlbumCovers");
-        sql::ResultSet *rs = ps->executeQuery();
-        REAlbumCoverIterator *it = new REAlbumCoverIterator(rs);
-        return it;
-    }
-    
 #pragma mark persistence
     
     int REAlbumCover::update() {
@@ -133,24 +126,5 @@ namespace soulsifter {
     
     const string& REAlbumCover::getThumbnail() const { return thumbnail; }
     void REAlbumCover::setThumbnail(const string& thumbnail) { this->thumbnail = thumbnail; }
-    
-# pragma mark RESongIterator
-    
-    REAlbumCover::REAlbumCoverIterator::REAlbumCoverIterator(sql::ResultSet* resultset) :
-    rs(resultset) {
-    }
-    
-    REAlbumCover::REAlbumCoverIterator::~REAlbumCoverIterator() {
-        delete rs;
-    }
-    
-    bool REAlbumCover::REAlbumCoverIterator::next(REAlbumCover* albumcover) {
-        if (rs->next()) {
-            populateFields(rs, albumcover);
-            return true;
-        } else {
-            return false;
-        }
-    }
     
 }
