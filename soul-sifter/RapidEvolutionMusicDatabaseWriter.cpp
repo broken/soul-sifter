375284a34f9cfd0f9283a4e23c561646df282951
Robby Neale <git@robertneale.com>
2013-05-18 22:53:01 -0600

    write re db from songs data instead of re songs

--- soul-sifter/RapidEvolutionMusicDatabaseWriter.cpp
+++ soul-sifter/RapidEvolutionMusicDatabaseWriter.cpp
@@ -22,6 +22,7 @@
 #include "RESong.h"
 #include "REXml.h"
 #include "ResultSetIterator.h"
+#include "Song.h"
 #include "Style.h"
 
 using namespace std;
@@ -149,112 +150,117 @@ void RapidEvolutionMusicDatabaseWriter::write() {
     w.startElement("songs", songsAttribs).endl();
     deleteAttribs(songsAttribs);
     dogatech::ResultSetIterator<RESong>* songs = RESong::findAll();
-    RESong song;
-    while (songs->next(&song)) {
-        song.setStylesBitmaskFromDb();
+    RESong reSong;
+    while (songs->next(&reSong)) {
+        reSong.setStylesBitmaskFromDb();
+        Song* song = Song::findByRESongId(reSong.getId());
         attrib* songAttribs[] = {
             new attrib("num_excludes","0"),
-            new attrib("num_mixouts",int2str(Mix::mixoutCountForRESongId(song.getId()))),
+            new attrib("num_mixouts",int2str(Mix::mixoutCountForRESongId(reSong.getId()))),
             NULL
         };
         w.startElement("song", songAttribs).endl();
         deleteAttribs(songAttribs);
         //f << "<unique_id>" << song.getUniqueId() << "</unique_id>" << endl;
-        w.startElement("unique_id", NULL).writeInt(song.getId()).endElement("unique_id");
+        w.startElement("unique_id", NULL).writeInt(reSong.getId()).endElement("unique_id");
         //f << "<songid_winfo>" << song.getSongIdWInfo() << "</songid_winfo>" << endl;
-        w.startElement("songid_winfo", NULL).writeChars(song.getSongidWinfo())->endElement("songid_winfo");
+        w.startElement("songid_winfo", NULL).writeChars(reSong.getSongidWinfo())->endElement("songid_winfo");
         //f << "<songid>" << song.getSongId() << "</songid>" << endl;
-        w.startElement("songid", NULL).writeChars(song.getSongid())->endElement("songid");
+        w.startElement("songid", NULL).writeChars(reSong.getSongid())->endElement("songid");
         //f << "<shortid>" << song.getShortId() << "</shortid>" << endl;
-        w.startElement("shortid", NULL).writeChars(song.getShortid())->endElement("shortid");
+        w.startElement("shortid", NULL).writeChars(reSong.getShortid())->endElement("shortid");
         //f << "<shortid_winfo>" << song.getShortIdWInfo() << "</shortid_winfo>" << endl;
-        w.startElement("shortid_winfo", NULL).writeChars(song.getShortidWinfo())->endElement("shortid_winfo");
-        if (song.getArtist().length() > 0)
+        w.startElement("shortid_winfo", NULL).writeChars(reSong.getShortidWinfo())->endElement("shortid_winfo");
+        if (song->getArtist().length() > 0)
             //f << "<artist>" << song.getArtist() << "</artist>" << endl;
-            w.startElement("artist", NULL).writeChars(song.getArtist())->endElement("artist");
-        if (song.getAlbum().length() > 0)
+            w.startElement("artist", NULL).writeChars(song->getArtist())->endElement("artist");
+        if (song->getAlbum()->getName().length() > 0)
             //f << "<album>" << song.getAlbum() << "</album>" << endl;
-            w.startElement("album", NULL).writeChars(song.getAlbum())->endElement("album");
-        if (song.getTrack().length() > 0)
+            w.startElement("album", NULL).writeChars(song->getAlbum()->getName())->endElement("album");
+        else
+            //f << "<album>" << song.getAlbum() << "</album>" << endl;
+            w.startElement("album", NULL).writeChars(song->getTitle())->endElement("album");
+        if (song->getTrack().length() > 0)
             //f << "<track>" << song.getTrack() << "</track>" << endl;
-            w.startElement("track", NULL).writeChars(song.getTrack())->endElement("track");
-        if (song.getTitle().length() > 0)
+            w.startElement("track", NULL).writeChars(song->getTrack())->endElement("track");
+        if (song->getTitle().length() > 0)
             //f << "<title>" << song.getTitle() << "</title>" << endl;
-            w.startElement("title", NULL).writeChars(song.getTitle())->endElement("title");
-        if (song.getRemix().length() > 0)
+            w.startElement("title", NULL).writeChars(song->getTitle())->endElement("title");
+        if (song->getRemixer().length() > 0)
             //f << "<remix>" << song.getRemix() << "</remix>" << endl;
-            w.startElement("remix", NULL).writeChars(song.getRemix())->endElement("remix");
-        if (song.getComments().length() > 0)
+            w.startElement("remix", NULL).writeChars(song->getRemixer())->endElement("remix");
+        if (song->getComments().length() > 0)
             //f << "<comments>" << song.getComments() << "</comments>" << endl;
-            w.startElement("comments", NULL).writeChars(song.getComments())->endElement("comments");
-        if (song.getFeaturing().length() > 0)
+            w.startElement("comments", NULL).writeChars(song->getComments())->endElement("comments");
+        if (reSong.getFeaturing().length() > 0)
             //f << "<custom1>" << song.getFeaturing() << "</custom1>" << endl;
-            w.startElement("custom1", NULL).writeChars(song.getFeaturing())->endElement("custom1");
-        if (song.getReleaseDate().length() > 0)
+            w.startElement("custom1", NULL).writeChars(reSong.getFeaturing())->endElement("custom1");
+        if (song->getAlbum()->getReleaseDateYear() != 0)
             //f << "<custom2>" << song.getReleaseDate() << "</custom2>" << endl;
-            w.startElement("custom2", NULL).writeChars(song.getReleaseDate())->endElement("custom2");
-        if (song.getLabel().length() > 0)
+            w.startElement("custom2", NULL).writeChars(song->getAlbum()->reReleaseDate())->endElement("custom2");
+        if (song->getAlbum()->getLabel().length() > 0)
             //f << "<custom3>" << song.getLabel() << "</custom3>" << endl;
-            w.startElement("custom3", NULL).writeChars(song.getLabel())->endElement("custom3");
-        if (song.getCatalogId().length() > 0)
+            w.startElement("custom3", NULL).writeChars(song->getAlbum()->getLabel())->endElement("custom3");
+        if (song->getAlbum()->getCatalogId().length() > 0)
             //f << "<custom4>" << song.getCatalogId() << "</custom4>" << endl;
-            w.startElement("custom4", NULL).writeChars(song.getCatalogId())->endElement("custom4");
-        if (song.getTime().length() > 0)
+            w.startElement("custom4", NULL).writeChars(song->getAlbum()->getCatalogId())->endElement("custom4");
+        if (reSong.getTime().length() > 0)
             //f << "<time>" << song.getTime() << "</time>" << endl;
-            w.startElement("time", NULL).writeChars(song.getTime())->endElement("time");
-        if (song.getTimeSignature().length() > 0)
+            w.startElement("time", NULL).writeChars(reSong.getTime())->endElement("time");
+        if (reSong.getTimeSignature().length() > 0)
             //f << "<time_signature>" << song.getTimeSignature() << "</time_signature>" << endl;
-            w.startElement("time_signature", NULL).writeChars(song.getTimeSignature())->endElement("time_signature");
-        if (song.getFilename().length() > 0)
+            w.startElement("time_signature", NULL).writeChars(reSong.getTimeSignature())->endElement("time_signature");
+        if (song->getFilepath().length() > 0)
             //f << "<filename>" << song.getFilename() << "</filename>" << endl;
-            w.startElement("filename", NULL).writeChars(song.getFilename())->endElement("filename");
-        if (song.getDigitalOnly().length() > 0)
+            w.startElement("filename", NULL).writeChars(song->getFilepath())->endElement("filename");
+        if (reSong.getDigitalOnly().length() > 0)
             //f << "<digital_only>" << song.getDigitalOnly() << "</digital_only>" << endl;
-            w.startElement("digital_only", NULL).writeChars(song.getDigitalOnly())->endElement("digital_only");
-        if (song.getDisabled().length() > 0)
+            w.startElement("digital_only", NULL).writeChars(reSong.getDigitalOnly())->endElement("digital_only");
+        if (reSong.getDisabled().length() > 0)
             //f << "<disabled>" << song.getDisabled() << "</disabled>" << endl;
-            w.startElement("disabled", NULL).writeChars(song.getDisabled())->endElement("disabled");
-        if (song.getCompilation().length() > 0)
+            w.startElement("disabled", NULL).writeChars(reSong.getDisabled())->endElement("disabled");
+        if (reSong.getCompilation().length() > 0)
             //f << "<compilation>" << song.getCompilation() << "</compilation>" << endl;
-            w.startElement("compilation", NULL).writeChars(song.getCompilation())->endElement("compilation");
-        if (song.getKeyStart().length() > 0)
+            w.startElement("compilation", NULL).writeChars(reSong.getCompilation())->endElement("compilation");
+        if (reSong.getKeyStart().length() > 0)
             //f << "<key_start>" << song.getKeyStart() << "</key_start>" << endl;
-            w.startElement("key_start", NULL).writeChars(song.getKeyStart())->endElement("key_start");
-        if (song.getKeyEnd().length() > 0)
+            w.startElement("key_start", NULL).writeChars(reSong.getKeyStart())->endElement("key_start");
+        if (reSong.getKeyEnd().length() > 0)
             //f << "<key_end>" << song.getKeyEnd() << "</key_end>" << endl;
-            w.startElement("key_end", NULL).writeChars(song.getKeyEnd())->endElement("key_end");
-        if (song.getKeyAccuracy() > 0)
+            w.startElement("key_end", NULL).writeChars(reSong.getKeyEnd())->endElement("key_end");
+        if (reSong.getKeyAccuracy() > 0)
             //f << "<key_accuracy>" << song.getKeyAccuracy() << "</key_accuracy>" << endl;
-            w.startElement("key_accuracy", NULL).writeInt(song.getKeyAccuracy()).endElement("key_accuracy");
-        if (song.getBpmStart().length() > 0)
+            w.startElement("key_accuracy", NULL).writeInt(reSong.getKeyAccuracy()).endElement("key_accuracy");
+        if (reSong.getBpmStart().length() > 0)
             //f << "<bpm_start>" << song.getBPMStart() << "</bpm_start>" << endl;
-            w.startElement("bpm_start", NULL).writeStr(song.getBpmStart()).endElement("bpm_start");
-        if (song.getBpmEnd().length() > 0)
+            w.startElement("bpm_start", NULL).writeStr(reSong.getBpmStart()).endElement("bpm_start");
+        if (reSong.getBpmEnd().length() > 0)
             //f << "<bpm_end>" << song.getBPMEnd() << "</bpm_end>" << endl;
-            w.startElement("bpm_end", NULL).writeStr(song.getBpmEnd()).endElement("bpm_end");
-        if (song.getBpmAccuracy() > 0)
+            w.startElement("bpm_end", NULL).writeStr(reSong.getBpmEnd()).endElement("bpm_end");
+        if (reSong.getBpmAccuracy() > 0)
             //f << "<bpm_accuracy>" << song.getBPMAccuracy() << "</bpm_accuracy>" << endl;
-            w.startElement("bpm_accuracy", NULL).writeInt(song.getBpmAccuracy()).endElement("bpm_accuracy");
-        if (song.getBeatIntensity() > 0)
+            w.startElement("bpm_accuracy", NULL).writeInt(reSong.getBpmAccuracy()).endElement("bpm_accuracy");
+        if (reSong.getBeatIntensity() > 0)
             //f << "<beat_intensity>" << song.getBeatIntensity() << "</beat_intensity>" << endl;
-            w.startElement("beat_intensity", NULL).writeInt(song.getBeatIntensity()).endElement("beat_intensity");
-        if (song.getReplayGain().length() > 0)
+            w.startElement("beat_intensity", NULL).writeInt(reSong.getBeatIntensity()).endElement("beat_intensity");
+        if (reSong.getReplayGain().length() > 0)
             //f << "<replay_gain>" << song.getReplayGain() << "</replay_gain>" << endl;
-            w.startElement("replay_gain", NULL).writeStr(song.getReplayGain()).endElement("replay_gain");
-        if (song.getNumPlays() > 0)
+            w.startElement("replay_gain", NULL).writeStr(reSong.getReplayGain()).endElement("replay_gain");
+        if (reSong.getNumPlays() > 0)
             //f << "<num_plays>" << song.getNumPlays() << "</num_plays>" << endl;
-            w.startElement("num_plays", NULL).writeInt(song.getNumPlays()).endElement("num_plays");
-        if (song.getRating() > 0)
+            w.startElement("num_plays", NULL).writeInt(reSong.getNumPlays()).endElement("num_plays");
+        if (song->getRating() > 0)
             //f << "<rating>" << song.getRating() << "</rating>" << endl;
-            w.startElement("rating", NULL).writeInt(song.getRating()).endElement("rating");
-        if (song.getDateAdded().length() > 0)
+            w.startElement("rating", NULL).writeInt(song->getRating()).endElement("rating");
+        if (song->getDateAdded() > 0)
             //f << "<date_added>" << song.getDateAdded() << "</date_added>" << endl;
-            w.startElement("date_added", NULL).writeChars(song.getDateAdded())->endElement("date_added");
-        if (song.getStylesBitmask().length() > 0)
+            w.startElement("date_added", NULL).writeChars(song->getDateAddedString())->endElement("date_added");
+        if (reSong.getStylesBitmask().length() > 0)
             //f << "<styles_bitmask>" << song.getStylesBitmask() << "</styles_bitmask>" << endl;
-            w.startElement("styles_bitmask", NULL).writeChars(song.getStylesBitmask())->endElement("styles_bitmask");
+            w.startElement("styles_bitmask", NULL).writeChars(reSong.getStylesBitmask())->endElement("styles_bitmask");
         //f << "</song>" << endl;
         w.endElement("song");
+        delete song;
     }
     delete songs;
     //f << "</songs>" << endl;
